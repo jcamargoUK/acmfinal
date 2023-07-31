1a394bd5a92342f4db163e86dcd1a655245fa345
Julio Camargo <jcamargo@hotmail.co.uk>
2023-07-31 20:06:52 +0100

    added read more...

--- components/sectionTwo.tsx
+++ components/sectionTwo.tsx
@@ -1,9 +1,181 @@
 "use client"
 
+import React, { useState } from 'react';
 import Image from 'next/image'
 
+interface SectionTwoProps {
+  // Add any props if needed
+}
 
-export default function SectionTwo() {
+const SectionTwo: React.FC<SectionTwoProps> = () => {
+  const [showFullContent, setShowFullContent] = useState(false);
+
+  const toggleContent = () => {
+    setShowFullContent((prev) => !prev);
+  };
+
+  const firstParagraph = (
+    <>
+      <p className='text-xl text-gray-600 px-4 text-left 
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> Our surveyors are trained to the highest level, with expertise spanning a variety of environments, including airports, rail and docks, commercial office spaces, redevelopment sites, residential properties, educational establishments, healthcare facilities, industrial premises, Ministry of Defence establishments, National Heritage sites, and secure establishments.
+      </p>
+    </>
+  );
+
+  const restOfContent = (
+    <>
+      <p className='text-xl text-gray-600 px-4 text-left 
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> Our surveyors are trained to the highest level, with expertise spanning a variety of environments, including airports, rail and docks, commercial office spaces, redevelopment sites, residential properties, educational establishments, healthcare facilities, industrial premises, Ministry of Defence establishments, National Heritage sites, and secure establishments.
+            </p>
+            <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> This includes comprehensive asbestos and hazardous material surveys, re-inspections, sampling surveys, and management of asbestos registers, development of robust management plans, formulating risk management strategies, preparing policy and procedure documents, managing procurement and overseeing remedial works, carrying out audits, conducting exposure assessments, and risk assessment and remedial strategies for asbestos in soils.
+            </p>
+    </>
+  );
+
+  const secondParagraph = (
+    <>
+       <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> Whilst our speciality lies in non-licensed asbestos removal, we acknowledge that some situations demand licensed removal.
+            </p>
+            <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> For such scenarios, we have established affiliations with fully licensed removal companies that share our commitment to safety and professionalism. 
+        </p>
+    </>
+  );
+
+  const secondContent = (
+    <>
+      <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> In choosing our services, you benefit from our extensive network and the simplicity of a single liaison point.  
+            </p>
+            <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10'>
+              Rest assured that we coordinate the optimal solution for all your asbestos-related requirements.
+       </p>
+    </>
+  )
+
+  const content = showFullContent ? (
+    <div className="content-transition overflow-hidden transition-max-height duration-200">
+      {firstParagraph}
+      {restOfContent}
+      <button className='text-blue-500' onClick={toggleContent}>
+        Read less
+      </button>
+    </div>
+  ) : (
+    <div className="content-transition overflow-hidden transition-max-height duration-200">
+      {firstParagraph}
+      <button className='text-blue-500' onClick={toggleContent}>
+        Read more
+      </button>
+    </div>
+  );
+
+  const contentTwo = showFullContent ? (
+    <div className="content-transition overflow-hidden transition-max-height duration-200">
+      {secondParagraph}
+      {secondContent}
+      <button className='text-blue-500' onClick={toggleContent}>
+        Read less
+      </button>
+    </div>
+  ) : (
+    <div className="content-transition overflow-hidden transition-max-height duration-200">
+      {secondParagraph}
+      <button className='text-blue-500' onClick={toggleContent}>
+        Read more
+      </button>
+    </div>
+  );
+
+  const thirdContent = (
+    <>
+      <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> Soft strip services play a vital role in the process of asbestos management. 
+            </p>
+            <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> Prior to refurbishment or demolition, non-structural elements are carefully removed from buildings. 
+            </p>
+            <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> Our expert team handles everything from internal fixtures and fittings to finishes down to the structural elements.
+            </p>
+    </>
+  )
+
+  const contentThee = (
+    <>
+      <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> This not only prepares the building for refurbishment or demolition but also allows for the identification and removal of any hidden hazardous material, such as asbestos. 
+            </p>
+            <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> The soft strip process goes hand in hand with our asbestos surveying and removal services, providing a comprehensive approach to maintaining a safe environment. 
+            </p>
+            <p className='text-xl text-gray-600 px-4 text-left
+            md:text-3xl
+            md:px-8
+            md:leading-10
+            '> Trust us to carry out these operations meticulously and safely, minimizing risk and disruption. 
+            </p>
+    </>
+  )
+
+  const contentThree = showFullContent ? (
+    <div className="content-transition overflow-hidden transition-max-height duration-200">
+      {thirdContent}
+      {contentThee}
+      <button className='text-blue-500' onClick={toggleContent}>
+        Read less
+      </button>
+    </div>
+  ) : (
+    <div className="content-transition overflow-hidden transition-max-height duration-200">
+      {thirdContent}
+      <button className='text-blue-500' onClick={toggleContent}>
+        Read more
+      </button>
+    </div>
+  );
+
+
+
+
   return (
     <section className='m-auto text-center'>
       <h1 className=' 
@@ -20,18 +192,7 @@ export default function SectionTwo() {
         <div>
           <div>
             <h2 className='text-gray-600 text-3xl pb-4 pt-6'>Asbestos Surveying</h2>
-            <p className='text-xl text-gray-600 px-4 text-left 
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> Our surveyors are trained to the highest level, with expertise spanning a variety of environments, including airports, rail and docks, commercial office spaces, redevelopment sites, residential properties, educational establishments, healthcare facilities, industrial premises, Ministry of Defence establishments, National Heritage sites, and secure establishments.
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> This includes comprehensive asbestos and hazardous material surveys, re-inspections, sampling surveys, and management of asbestos registers, development of robust management plans, formulating risk management strategies, preparing policy and procedure documents, managing procurement and overseeing remedial works, carrying out audits, conducting exposure assessments, and risk assessment and remedial strategies for asbestos in soils.
-            </p>
+            {content}
           </div>
           <div>
             <h2 className='text-gray-600 text-3xl pb-4 pt-6'>Non-Licensed Asbestos Removal</h2>
@@ -44,30 +205,8 @@ export default function SectionTwo() {
           </div>
           <div>
             <h2 className='text-gray-600 text-3xl pb-4 pt-6'>Associated Licensed Asbestos Removal</h2>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> Whilst our speciality lies in non-licensed asbestos removal, we acknowledge that some situations demand licensed removal.
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> For such scenarios, we have established affiliations with fully licensed removal companies that share our commitment to safety and professionalism. 
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> In choosing our services, you benefit from our extensive network and the simplicity of a single liaison point.  
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10'>
-              Rest assured that we coordinate the optimal solution for all your asbestos-related requirements.
-            </p>
+            {contentTwo}
+            
           </div>
           <div>
             <h2 className='text-gray-600 text-3xl pb-4 pt-6'>Air Monitoring</h2>
@@ -86,42 +225,8 @@ export default function SectionTwo() {
           </div>
           <div>
             <h2 className='text-gray-600 text-3xl pb-4 pt-6'>Soft Strip Works</h2>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> Soft strip services play a vital role in the process of asbestos management. 
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> Prior to refurbishment or demolition, non-structural elements are carefully removed from buildings. 
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> Our expert team handles everything from internal fixtures and fittings to finishes down to the structural elements.
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> This not only prepares the building for refurbishment or demolition but also allows for the identification and removal of any hidden hazardous material, such as asbestos. 
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> The soft strip process goes hand in hand with our asbestos surveying and removal services, providing a comprehensive approach to maintaining a safe environment. 
-            </p>
-            <p className='text-xl text-gray-600 px-4 text-left
-            md:text-3xl
-            md:px-8
-            md:leading-10
-            '> Trust us to carry out these operations meticulously and safely, minimizing risk and disruption. 
-            </p>
+            {contentThree}
+            
           </div>
           <div>
             <h2 className='text-gray-600 text-3xl pb-4 pt-6'>Lead Testing</h2>
@@ -176,3 +281,5 @@ export default function SectionTwo() {
     </section>
   )
 }
+
+export default SectionTwo
